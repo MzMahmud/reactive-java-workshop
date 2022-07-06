ce205f28d1f405932b25c0a8f8fac0e8599ad37b
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2022-07-06 10:26:40 +0600

    solved Exercise4

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -7,12 +7,14 @@ public class Exercise4 {
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumberMono()
-
         // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        ReactiveSources.intNumberMono()
+                       .subscribe(System.out::println);
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        var integer = ReactiveSources.intNumberMono()
+                                     .block();
+        System.out.println(integer);
 
         System.out.println("Press a key to end");
         System.in.read();
