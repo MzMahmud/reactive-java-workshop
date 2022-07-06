99273b6fb40fb8092b3f1e78f800c065b5c56fd6
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2022-07-06 12:20:16 +0600

    removed sout and commented dividor

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -9,23 +9,24 @@ public class Exercise1 {
 
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
-        System.out.println("Print all numbers in the intNumbersStream stream");
+        // Print all numbers in the intNumbersStream stream
         StreamSources.intNumbersStream()
                      .forEach(System.out::println);
 
-        System.out.println("Print numbers from intNumbersStream that are less than 5");
+        // Print numbers from intNumbersStream that are less than 5
         StreamSources.intNumbersStream()
                      .filter(n -> n < 5)
                      .forEach(System.out::println);
 
-        System.out.println("Print the second and third numbers in intNumbersStream that's greater than 5");
+        // Print the second and third numbers in intNumbersStream that's greater than 5
         StreamSources.intNumbersStream()
                      .filter(n -> n > 5)
                      .skip(1)
                      .limit(2)
                      .forEach(System.out::println);
 
-        System.out.println("Print the first number in intNumbersStream that's greater than 5. If nothing is found, print -1");
+        // Print the first number in intNumbersStream that's greater than 5.
+        // If nothing is found, print -1
         var firstNumberGreaterThan5OrMinus1 =
                 StreamSources.intNumbersStream()
                              .filter(n -> n > 5)
@@ -33,19 +34,25 @@ public class Exercise1 {
                              .orElse(-1);
         System.out.println(firstNumberGreaterThan5OrMinus1);
 
-        System.out.println("Print first names of all users in userStream");
+        // Print first names of all users in userStream
         StreamSources.userStream()
                      .map(User::getFirstName)
                      .forEach(System.out::println);
 
-        System.out.println("Print first names in userStream for users that have IDs from number stream");
-        System.out.println("flatMap");
+        // Print first names in userStream for users that have IDs from number stream
+        // flatMap
         StreamSources.intNumbersStream()
                      .flatMap(id -> StreamSources.userStream().filter(user -> Objects.equals(user.getId(), id)))
                      .map(User::getFirstName)
                      .forEach(System.out::println);
 
-        System.out.println("optimized with idSet");
+        // anyMatch
+        StreamSources.userStream()
+                     .filter(user -> StreamSources.intNumbersStream().anyMatch(id -> Objects.equals(id, user.getId())))
+                     .map(User::getFirstName)
+                     .forEach(System.out::println);
+
+        // optimized with idSet
         var idSet = StreamSources.intNumbersStream()
                                  .collect(Collectors.toUnmodifiableSet());
         StreamSources.userStream()
--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -8,11 +8,11 @@ public class Exercise2 {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
-        System.out.println("Print all numbers in the ReactiveSources.intNumbersFlux stream");
+        // Print all numbers in the ReactiveSources.intNumbersFlux stream
         ReactiveSources.intNumbersFlux()
                        .subscribe(System.out::println);
 
-        System.out.println("Print all users in the ReactiveSources.userFlux stream");
+        // Print all users in the ReactiveSources.userFlux stream
         ReactiveSources.userFlux()
                        .subscribe(System.out::println);
 
